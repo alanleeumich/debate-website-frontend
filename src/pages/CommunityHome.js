0b8bf11361e64b5ed0e54ca0e98036ea58fec7c3
alanleeumich <alanleee@umich.edu>
2022-10-10 14:00:40 -0400

    current prompt shown

--- src/pages/CommunityHome.js
+++ src/pages/CommunityHome.js
@@ -1,12 +1,30 @@
 import { useNavigate } from "react-router-dom";
 import { useParams } from 'react-router-dom'
-
+import { useState } from "react";
+import { useEffect } from "react";
 
 const CommunityHome = () =>{
 
     const navigate = useNavigate();
     const { community } = useParams()
+    const [prompt, setPrompt] = useState("");
     const api = "https://squabblegoblin-backend.herokuapp.com"
+
+    let getPrompt = async() =>{
+        let response = await fetch(api + '/api/get-prompt',{
+          method: "PUT",
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({"community" : community})
+          
+        })
+        let data = await response.json()
+        console.log(data)
+        setPrompt(data)
+        
+    }
+
 
     let joinRoomAff = async() =>{
         let response = await fetch(api + '/api/join',{
@@ -35,12 +53,15 @@ const CommunityHome = () =>{
         
     }
     
+    useEffect(() =>{
+        getPrompt()
+    },[])
 
     return (
         <div className="App">
             <div className="homePanel">
                 <div className = "prompt">
-                    <h1>pineapple belongs on pizza</h1>
+                    <h1>{prompt}</h1>
                 </div>
                 <div className="buttons">
                     <button id = "agree" onClick={joinRoomAff}>agree</button>
